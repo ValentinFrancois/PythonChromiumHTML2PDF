c12c1c25f48577492bad1d168d12c56932212edd
vfrancois <v.francois@mediaire.de>
2022-01-15 00:58:16 +0100

    fix lint rule

--- package/PythonChromiumHTML2PDF/__init__.py
+++ package/PythonChromiumHTML2PDF/__init__.py
@@ -1,3 +1,5 @@
+__all__ = ['ChromeProcess', 'ChromeApi', 'ChromeApiCallback', 'print_to_pdf']
+
 from PythonChromiumHTML2PDF.chrome_process import ChromeProcess
 from PythonChromiumHTML2PDF.chrome_api import ChromeApi, ChromeApiCallback
 from PythonChromiumHTML2PDF.print_to_pdf import print_to_pdf
