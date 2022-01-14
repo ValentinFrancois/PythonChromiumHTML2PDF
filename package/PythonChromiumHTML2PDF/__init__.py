a00674d6546a6a7b23a0efac2c5dbcb8512a0d8a
vfrancois <v.francois@mediaire.de>
2022-01-14 02:39:10 +0100

    working package install in docker

---
+++ package/PythonChromiumHTML2PDF/__init__.py
@@ -0,0 +1,3 @@
+from PythonChromiumHTML2PDF.chrome_process import ChromeProcess
+from PythonChromiumHTML2PDF.chrome_api import ChromeApi, ChromeApiCallback
+from PythonChromiumHTML2PDF.print_to_pdf import print_to_pdf
