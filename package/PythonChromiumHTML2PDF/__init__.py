ca84e913375d40bb031886fc04b64324e62117c1
vfrancois <v.francois@mediaire.de>
2022-01-15 01:56:43 +0100

    add more package info

--- package/PythonChromiumHTML2PDF/__init__.py
+++ package/PythonChromiumHTML2PDF/__init__.py
@@ -1,3 +1,6 @@
+"""HTML to PDF converter using Chromium via the Chrome DevTools protocol"""
+
+__version__ = '0.1.0'
 __all__ = ['ChromeProcess', 'ChromeApi', 'ChromeApiCallback', 'print_to_pdf']
 
 from PythonChromiumHTML2PDF.chrome_process import ChromeProcess
